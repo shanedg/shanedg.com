5b1908def86a45c3e8d72387a7f73c36f50704c6
Shane Garrity <shanedgarrity@gmail.com>
2018-08-25 22:52:12 -0400

    complete transition to react-motion, remove react-addons-css-transition-group

--- src/components/Employer/Employer.js
+++ src/components/Employer/Employer.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
-// import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-// import ReactMotion, { StaggeredMotion, spring } from 'react-motion';
-// import { StaggeredMotion, spring } from 'react-motion';
-import { Motion, StaggeredMotion, spring } from 'react-motion';
+import { StaggeredMotion, spring, presets } from 'react-motion';
+import range from 'lodash.range';
 
 import './Employer.css';
 import SimpleDateRange from './SimpleDateRange';
-import { height } from 'window-size';
 
 class Employer extends Component {
   constructor(props) {
@@ -34,65 +31,65 @@ class Employer extends Component {
         <div className="role-body">
           <span className="role-heading">
             <button className="button toggle-role-details" onClick={this.toggleRoleDetails}>{this.state.open ? '<' : '>'}</button>
-
             <h4 className="h4 role-title">{this.props.jobDetails.roleTitle}</h4>
-            {/* <p className="p tldr"><strong className="strong tldr">tl;dr</strong> &mdash; {this.props.jobDetails.highlightsBlurb}</p> */}
-            
           </span>
 
           {this.state.open &&
             this.props.jobDetails.responsibilities &&
-            <StaggeredMotion
-              // defaultStyles={[{h: 0}, {h: 0}, {h: 0}]}
-              defaultStyles={[{ x: -100}, { x: -100}, { x: -100}]}
-              styles={prevInterpolatedStyles => prevInterpolatedStyles.map((_, i) => {
-                return i === 0
-                  ? {x: spring(0)}
-                  : {x: spring(prevInterpolatedStyles[i - 1].x)}
-              })}>
-              {interpolatingStyles =>
-                <div>
-                  {interpolatingStyles.map((style, i) => {
-                    return (
-                      <div key={i} style={{border: '1px solid', height: '20px', transform: `translateX(${style.x}vh`}} />
-                    )})
-                  }
-                </div>
-              }
-            </StaggeredMotion>
+            <ul>
+              <StaggeredMotion
+                defaultStyles={range(this.props.jobDetails.responsibilities.length).map(() => ({ x: -100}))}
+                styles={prevInterpolatedStyles => prevInterpolatedStyles.map((_, i) => {
+                  return i === 0
+                    ? {x: spring(0)}
+                    : {x: spring(prevInterpolatedStyles[i - 1].x, presets.stiff)}
+                })}>
+                {interpolatingStyles =>
+                  <section className="section responsibilities">
+                    <h5>responsibilities:</h5>
+                    <ul>
+                      {interpolatingStyles.map((style, i) => {
+                        return (
+                          <li key={i} style={{transform: `translateX(${style.x}vw`}}>
+                            {this.props.jobDetails.responsibilities[i]}
+                          </li>
+                        )})
+                      }
+                    </ul>
+                  </section>
+                }
+              </StaggeredMotion>
+            </ul>
           }
 
-          {/* <ReactCSSTransitionGroup
-            transitionName="role-details"
-            transitionAppear={true}
-            transitionAppearTimeout={750}
-            transitionEnterTimeout={750}
-            transitionLeaveTimeout={750}>
-              {this.state.open &&
-                this.props.jobDetails.responsibilities &&
-                <section className="section responsibilities">
-                  <h5>responsibilities:</h5>
-                  <ul>
-                    {this.props.jobDetails.responsibilities.map((responsibilityDescription, index) => {
-                      return <li key={index}>{responsibilityDescription}</li>
-                    })}
-                  </ul>
-                </section>
-              }
+          {this.state.open &&
+            this.props.jobDetails.projects &&
+            <ul>
+              <StaggeredMotion
+                defaultStyles={range(this.props.jobDetails.projects.length).map(() => ({ x: -100}))}
+                styles={prevInterpolatedStyles => prevInterpolatedStyles.map((_, i) => {
+                  return i === 0
+                    ? {x: spring(0)}
+                    : {x: spring(prevInterpolatedStyles[i - 1].x, presets.stiff)}
+                })}>
+                {interpolatingStyles =>
+                  <section className="section projects">
+                    <h5>projects:</h5>
+                    <ul>
+                      {interpolatingStyles.map((style, i) => {
+                        return (
+                          <li key={i} style={{transform: `translateX(${style.x}vw`}}>
+                            {this.props.jobDetails.projects[i]}
+                          </li>
+                        )})
+                      }
+                    </ul>
+                  </section>
+                }
+              </StaggeredMotion>
+            </ul>
+          }
 
-              {this.state.open &&
-                this.props.jobDetails.projects &&
-                <section className="section projects">
-                  <h5>projects:</h5>
-                  <ul>
-                    {this.props.jobDetails.projects.map((projectDescription, index) => {
-                      return <li key={index}>{projectDescription}</li>
-                    })}
-                  </ul>
-                </section>
-              }
-            </ReactCSSTransitionGroup> */}
-          
         </div>
       </section>
     );
