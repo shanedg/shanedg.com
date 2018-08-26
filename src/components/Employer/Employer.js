83ac1028155770ec4f2c2b643b4df044b7bd4e6e
Shane Garrity <shanedgarrity@gmail.com>
2018-08-26 00:31:54 -0400

    figuring out TransitionMotion

--- src/components/Employer/Employer.js
+++ src/components/Employer/Employer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { StaggeredMotion, spring, presets } from 'react-motion';
+// import { StaggeredMotion, spring, presets } from 'react-motion';
+import { TransitionMotion, StaggeredMotion, spring, presets } from 'react-motion';
 import range from 'lodash.range';
 
 import './Employer.css';
@@ -9,16 +10,81 @@ class Employer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      open: false
+      open: false,
+
+      // items: [{
+      //   key: 'a', size: 100
+      // }, {
+      //   key: 'b', size: 100
+      // }, {
+      //   key: 'c', size: 100
+      // }]
+
+      // items: [{
+      //   key: 'a', size: 100
+      // }]
+
+      items: []
+
     };
 
     this.toggleRoleDetails = this.toggleRoleDetails.bind(this);
+    this.test = this.test.bind(this);
   }
 
   toggleRoleDetails() {
     this.setState((prevState) => ({
       open: !prevState.open
     }));
+  }
+
+  willLeave() {
+    return {
+      height: spring(0, presets.stiff)
+    };
+  }
+
+  willEnter() {
+    return {
+      height: 0
+    }
+  }
+
+  getDefaultStyles = () => {
+    return this.state.items.map(item => ({
+      ...item,
+      style: {
+        height: 0
+      }
+    }));
+  }
+
+  getStyles = () => {
+    const {items} = this.state;
+
+    return items.map((item, i) => {
+      return {
+        ...item,
+        style: {
+          // height: spring(100, presets.stiff)
+          height: spring(item.size, presets.stiff)
+        }
+      };
+    });
+  }
+
+  test() {
+    this.setState({
+      items: [{
+        key: 'responsibilities', size: 100
+      }, {
+        key: 'projects', size: 100
+      }]
+    });
+  }
+
+  componentDidMount() {
+    this.test();
   }
 
   render() {
@@ -34,8 +100,26 @@ class Employer extends Component {
             <h4 className="h4 role-title">{this.props.jobDetails.roleTitle}</h4>
           </span>
 
-          {this.state.open &&
-            this.props.jobDetails.responsibilities &&
+          <button onClick={this.test}>duh</button>
+          <TransitionMotion
+            defaultStyles={this.getDefaultStyles()}
+            willLeave={this.willLeave}
+            willEnter={this.willEnter}
+            styles={this.getStyles()}
+            >
+            {interpolatedStyles => 
+              <div>
+                {interpolatedStyles.map((config, i) => {
+                  return <div key={config.key} style={{...config.style, border: '1px solid'}}>
+                    {config.key}
+                  </div>
+                })}
+              </div>
+            }
+          </TransitionMotion>
+
+          {/* {this.state.open && */}
+            {this.props.jobDetails.responsibilities &&
             <ul>
               <StaggeredMotion
                 defaultStyles={range(this.props.jobDetails.responsibilities.length).map(() => ({ x: -100}))}
@@ -62,8 +146,8 @@ class Employer extends Component {
             </ul>
           }
 
-          {this.state.open &&
-            this.props.jobDetails.projects &&
+          {/* {this.state.open && */}
+            {this.props.jobDetails.projects &&
             <ul>
               <StaggeredMotion
                 defaultStyles={range(this.props.jobDetails.projects.length).map(() => ({ x: -100}))}
