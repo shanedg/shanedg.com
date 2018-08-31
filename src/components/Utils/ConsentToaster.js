1eaeca42f6ede24c20c01895e185fd119e076eff
Shane Garrity <shanedgarrity@gmail.com>
2018-08-30 23:50:55 -0400

    fiddle with consent toaster a little

--- src/components/Utils/ConsentToaster.js
+++ src/components/Utils/ConsentToaster.js
@@ -24,19 +24,12 @@ class ConsentToaster extends Component {
     return (
       <div className="consent-toaster-wrap">
         <div className="consent-toaster" ref={this.toasterDOM}>
+          <p>This site may use Cookies. Do you consent to allowing this site to store Cookies on your device/browser? <a href="https://en.wikipedia.org/wiki/HTTP_cookie" target="_blank" rel="noopener noreferrer">More about Cookies</a></p>
+          <span className="span consent-choice">
+            <button className="button" id="cookie-consent-yes" onClick={this.props.consentHandler}>Yes</button>
+            <button className="button" id="cookie-consent-no" onClick={this.props.consentHandler}>No</button>
+          </span>
           <button className="button dismiss-toaster" id="dismiss-consent-toaster" onClick={this.props.dismissHandler}>X</button>
-          <p>This site may use Cookies. Do you consent to allowing this site to store Cookies on your device/browser?</p>
-          <div className="consent-options">
-            <span className="span consent-choice">
-              <button className="button" id="cookie-consent-yes" onClick={this.props.consentHandler}>Yes</button>
-              <button className="button" id="cookie-consent-no" onClick={this.props.consentHandler}>No</button>
-            </span>
-            <span className="span">
-              <aside className="aside">
-                <a href="https://en.wikipedia.org/wiki/HTTP_cookie" target="_blank" rel="noopener noreferrer">More about Cookies</a>
-              </aside>
-            </span>
-          </div>
         </div>
       </div>
     );
