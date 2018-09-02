32dd483691cddbd58b7e3e3209fa87e709451d85
Shane Garrity <shanedgarrity@gmail.com>
2018-09-01 22:25:01 -0400

    actually mostly darker blue blocks

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,7 +9,7 @@ class Footer extends Component {
         <h2>talk to me</h2>
         <p>check out my <a href="https://github.com/shanedgarrity" target="_blank" rel="noopener noreferrer">github</a> or connect with me on <a href="https://www.linkedin.com/in/shane-garrity-ba915544" target="_blank" rel="noopener noreferrer">linkedin</a> or shoot me an <a href="mailto:shanedgarrity@gmail.com">email</a></p>
         <h3>*privacy</h3>
-        <p>for information about what personal data this site collects, see the privacy policy:
+        <p>for information about what personal data this site collects:&nbsp;
           {/* <!-- Begin iubenda generated privacy policy --> */}
           <a href="https://www.iubenda.com/privacy-policy/68672540" className="iubenda-black iubenda-embed " title="Privacy Policy">privacy policy</a>
           {/* <!-- End iubenda generated privacy policy --> */}
