195c7ca3118e0384afd8af5c8ff04606074417dd
Shane Garrity <shanedgarrity@gmail.com>
2018-09-03 03:05:19 -0400

    add preferences consent toggle behavior and styles

--- src/components/Preferences/Preferences.js
+++ src/components/Preferences/Preferences.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import './Preferences.css';
 
 class Preferences extends Component {
+  constructor(props) {
+    super(props);
+    this.consentToggle = this.consentToggle.bind(this);
+  }
+
+  consentToggle(e) {
+    const status = e.currentTarget.checked;
+    this.props.updateConsent(status);
+
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      'event': `cookie_consent_update_${status ? 'granted' : 'revoked'}`
+    });
+  }
 
   render() {
 
     return (
-      <div></div>
+      <article className="article preferences">
+        <h2 className="h2">preferences</h2>
+
+        <label htmlFor="cookies-opt-in">Opt in to this site's cookies:</label>
+        <input type="checkbox" name="cookies-opt-in" id="cookies-opt-in" onChange={this.consentToggle} checked={this.props.consent.consentGranted} />
+
+        {this.props.consent.priorConsent &&
+          (
+            this.props.consent.consentGranted ?
+            <p><strong>You're opted-in to cookies.</strong></p> :
+            <p><strong>You're opted-out of cookies.</strong></p>
+          )
+        }
+        
+      </article>
     );
   }
 }
