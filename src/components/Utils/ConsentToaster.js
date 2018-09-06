8bed7d7b1c3d005f2024654a9a13c1fb800de6b1
Shane Garrity <shanedgarrity@gmail.com>
2018-09-05 22:16:38 -0400

    resolve bad symmetry of consent toaster dismiss icon, prefer exit 'tab'

--- src/components/Utils/ConsentToaster.js
+++ src/components/Utils/ConsentToaster.js
@@ -22,16 +22,14 @@ class ConsentToaster extends Component {
   render() {
 
     return (
-      <div className="consent-toaster-wrap">
-        <div className="consent-toaster" ref={this.toasterDOM}>
+      <div className="Consent-Toaster">
+        <button className="button dismiss-toaster" id="dismiss-consent-toaster" onClick={this.props.dismissHandler}>X</button>
+        <div className="toaster-body" ref={this.toasterDOM}>
           <p>This site uses <a href="https://en.wikipedia.org/wiki/HTTP_cookie" target="_blank" rel="noopener noreferrer" title="read more about 'Cookies'">Cookies</a>; do you consent to allowing this site to store Cookies on your device/browser?</p>
           <span className="span consent-choice">
             <button className="button" id="cookie-consent-yes" onClick={this.props.consentHandler}>Yes</button>
             <button className="button" id="cookie-consent-no" onClick={this.props.consentHandler}>No</button>
           </span>
-          <button className="button dismiss-toaster" id="dismiss-consent-toaster" onClick={this.props.dismissHandler}>
-            <span>X</span>
-          </button>
         </div>
       </div>
     );
