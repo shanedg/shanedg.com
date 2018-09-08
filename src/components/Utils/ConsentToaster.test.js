9acc15965f0d6ba0ffbb33b4f3364b166d198033
Shane Garrity <shanedgarrity@gmail.com>
2018-09-08 18:42:58 -0400

    Anything using <Link /> must render as child of Router

--- src/components/Utils/ConsentToaster.test.js
+++ src/components/Utils/ConsentToaster.test.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ConsentToaster from './ConsentToaster';
 
+// Uses <Link /> so must render inside Router context
+import {BrowserRouter} from 'react-router-dom';
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<ConsentToaster />, div);
+  ReactDOM.render(
+    <BrowserRouter><ConsentToaster /></BrowserRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
