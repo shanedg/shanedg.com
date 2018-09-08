fd9335ff219b0c5c422a0f5fef586721462f1fad
Shane Garrity <shanedgarrity@gmail.com>
2018-09-08 17:51:58 -0400

    resolve privacy policy embed script during privacy component mount

--- src/components/Privacy/Privacy.js
+++ src/components/Privacy/Privacy.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 import './Privacy.css';
 
 class Privacy extends Component {
+  componentDidMount() {
+    // Only in browser context:
+    if (window && document) {
+      this.iubendaClientSideLoader(document);
+    }
+  }
+
+  // Create iubenda generated privacy policy embed script
+  iubendaClientSideLoader(d) {
+    var s = d.createElement("script"),
+      tag = d.getElementsByTagName("body")[0];
+    s.src="https://cdn.iubenda.com/iubenda.js";
+    tag.appendChild(s);
+  }
 
   render() {
    return (
@@ -12,13 +26,6 @@ class Privacy extends Component {
           <a href="https://www.iubenda.com/privacy-policy/68672540" className="iubenda-black iubenda-embed " title="Privacy Policy">privacy policy</a>
           {/* <!-- End iubenda generated privacy policy --> */}
         </p>
-
-        {/* <!-- Begin iubenda generated privacy policy embed script --> */}
-        {/* <script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script> */}
-        <script type="text/javascript" dangerouslySetInnerHTML={{
-          __html: `(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);`
-        }} />
-        {/* <!-- End iubenda generated privacy policy embed script --> */}
       </section>
     );
   }
