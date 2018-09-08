15430cf9d3aa28f043057c1ef8db54e85a82a555
Shane Garrity <shanedgarrity@gmail.com>
2018-09-08 01:09:47 -0400

    improve role detail toggle and animation

--- src/components/Work/Employer/Employer.js
+++ src/components/Work/Employer/Employer.js
@@ -27,8 +27,7 @@ class Employer extends Component {
       } else {
         return ({
           items: [{
-            key: 'responsibilities',
-            size: 1000
+            key: 'ask-me-about'
           }],
           open: true
         })
@@ -39,12 +38,14 @@ class Employer extends Component {
 
   willLeave() {
     return {
-      maxHeight: spring(0, presets.noWobble),
+      transform: spring(0, presets.noWobble),
+      maxHeight: spring(0, presets.noWobble)
     };
   }
 
   willEnter() {
     return {
+      transform: 0,
       maxHeight: 0
     }
   }
@@ -53,6 +54,7 @@ class Employer extends Component {
     return this.state.items.map(item => ({
       ...item,
       style: {
+        transform: 0,
         maxHeight: 0
       }
     }));
@@ -65,7 +67,8 @@ class Employer extends Component {
       return {
         ...item,
         style: {
-          maxHeight: spring(2000, presets.stiff)
+          transform: spring(1, presets.stiff),
+          maxHeight: 200
         }
       };
     });
@@ -81,13 +84,11 @@ class Employer extends Component {
         
         <div className="role-body">
           <span className="role-heading">
-            <button className={`button toggle-role-details${this.state.open ? ' toggle-open' : ''}`} onClick={this.toggleRoleDetails} style={{
-              animationDelay: (500 + (this.props.order * 1050)) + 'ms'
-              }}>
+            <button className={`button toggle-role-details${this.state.open ? ' toggle-open' : ''}`} onClick={this.toggleRoleDetails}>
               <span>
                 {this.state.open ? '-' : '+'}
               </span>
-            </button>&nbsp;
+            </button>
             <h4 className="h4 role-title">{this.props.jobDetails.roleTitle}</h4>
           </span>
 
@@ -101,61 +102,32 @@ class Employer extends Component {
               <div>
                 {interpolatedStyles.map((config, i) => {
                   return <div key={config.key} style={{
-                    ...config.style,
+                    transform: `scaleY(${config.style.transform})`,
+                    maxHeight: config.style.maxHeight + 'px',
                     overflow: 'hidden'
                   }}>
-                    {this.props.jobDetails.responsibilities &&
-                      <ul>
-                        <StaggeredMotion
-                          defaultStyles={range(this.props.jobDetails.responsibilities.length).map(() => ({ x: -100}))}
-                          styles={prevInterpolatedStyles => prevInterpolatedStyles.map((_, i) => {
-                            return i === 0
-                              ? {x: spring(0)}
-                              : {x: spring(prevInterpolatedStyles[i - 1].x, presets.stiff)}
-                          })}>
-                          {interpolatingStyles =>
-                            <section className="section responsibilities">
-                              <h5>responsibilities:</h5>
-                              <ul>
-                                {interpolatingStyles.map((style, i) => {
-                                  return (
-                                    <li key={i} style={{transform: `translateX(${style.x}vw`}}>
-                                      {this.props.jobDetails.responsibilities[i]}
-                                    </li>
-                                  )})
-                                }
-                              </ul>
-                            </section>
-                          }
-                        </StaggeredMotion>
-                      </ul>
-                    }
-
-                    {this.props.jobDetails.projects &&
-                      <ul>
-                        <StaggeredMotion
-                          defaultStyles={range(this.props.jobDetails.projects.length).map(() => ({ x: -100}))}
-                          styles={prevInterpolatedStyles => prevInterpolatedStyles.map((_, i) => {
-                            return i === 0
-                              ? {x: spring(0)}
-                              : {x: spring(prevInterpolatedStyles[i - 1].x, presets.stiff)}
-                          })}>
-                          {interpolatingStyles =>
-                            <section className="section projects">
-                              <h5>projects:</h5>
-                              <ul>
-                                {interpolatingStyles.map((style, i) => {
-                                  return (
-                                    <li key={i} style={{transform: `translateX(${style.x}vw`}}>
-                                      {this.props.jobDetails.projects[i]}
-                                    </li>
-                                  )})
-                                }
-                              </ul>
-                            </section>
-                          }
-                        </StaggeredMotion>
-                      </ul>
+                    {this.props.jobDetails.askMeAbout &&
+                      <StaggeredMotion
+                        defaultStyles={range(this.props.jobDetails.askMeAbout.length).map(() => ({ x: -100}))}
+                        styles={prevInterpolatedStyles => prevInterpolatedStyles.map((_, i) => {
+                          return i === 0
+                            ? {x: spring(0)}
+                            : {x: spring(prevInterpolatedStyles[i - 1].x, presets.stiff)}
+                        })}>
+                        {interpolatingStyles =>
+                          <section className="section responsibilities">
+                            <ul>
+                              {interpolatingStyles.map((style, i) => {
+                                return (
+                                  <li key={i} style={{transform: `translateX(${style.x}vw`}}>
+                                    {this.props.jobDetails.askMeAbout[i]}
+                                  </li>
+                                )})
+                              }
+                            </ul>
+                          </section>
+                        }
+                      </StaggeredMotion>
                     }
                   </div>
                 })}
