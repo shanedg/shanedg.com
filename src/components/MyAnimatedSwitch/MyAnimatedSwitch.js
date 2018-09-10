22aa2432dc0e926874e5a6b6fb18dc011f4e059a
Shane Garrity <shanedgarrity@gmail.com>
2018-09-09 20:34:48 -0400

    possible to extend component behavior? hoc's?

---
+++ src/components/MyAnimatedSwitch/MyAnimatedSwitch.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import { AnimatedSwitch } from 'react-router-transition';
+
+class MyAnimatedSwitch extends AnimatedSwitch {
+  constructor(props) {
+    super(props);
+  }
+}
+
+export default MyAnimatedSwitch;
