51fe7f2fcf8e4145094695098b051e407a321414
Shane Garrity <shanedgarrity@gmail.com>
2018-09-10 00:50:11 -0400

    performa snackbar padding update on resize, extract this work from mount phase into own method

--- src/App.js
+++ src/App.js
@@ -31,12 +31,22 @@ class App extends Component {
 
     this.toasterRef = React.createRef();
 
+    this.adjustPadding = this.adjustPadding.bind(this);
     this.updateConsent = this.updateConsent.bind(this);
     this.consentHandler = this.consentHandler.bind(this);
     this.dismissHandler = this.dismissHandler.bind(this);
   }
 
   componentDidMount() {
+    this.adjustPadding();
+    window.addEventListener('resize', this.adjustPadding);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.adjustPadding);
+  }
+
+  adjustPadding() {
     if (!this.state.consent.alreadyAsked) {
       this.setState({
         extraPadding: {
