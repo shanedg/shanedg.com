ebccb327683b98c77b3237e6f20a129b7dbfd978
Shane Garrity <shanedgarrity@gmail.com>
2018-09-17 00:01:09 -0400

    replace old prefs link with privacy, lowercase

--- src/components/Utils/ConsentToaster.js
+++ src/components/Utils/ConsentToaster.js
@@ -13,7 +13,7 @@ class ConsentToaster extends Component {
       <div className="Consent-Toaster">
         <button className="button dismiss-toaster" id="dismiss-consent-toaster" onClick={this.props.dismissHandler}>X</button>
         <div className="toaster-body" ref={this.props.forwardedRef}>
-          <p>This site uses <a href="https://en.wikipedia.org/wiki/HTTP_cookie" target="_blank" rel="noopener noreferrer" title="read more about Cookies">Cookies</a>; do you consent to allowing this site to store Cookies on your device? You can always change your mind later <Link to="/preferences">here</Link>.</p>
+          <p>this site uses <a href="https://en.wikipedia.org/wiki/HTTP_cookie" target="_blank" rel="noopener noreferrer" title="read more about Cookies">cookies</a>; do you consent to allowing this site to store cookies on your device? you can always change your mind <Link to="/privacy/">later</Link>.</p>
           <span className="span consent-choice">
             <button className="button" id="cookie-consent-yes" onClick={this.props.consentHandler}>Yes</button>
             <button className="button" id="cookie-consent-no" onClick={this.props.consentHandler}>No</button>
