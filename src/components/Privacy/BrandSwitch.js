56a4252b61fe2cf17bc2749f8465095065ef50d2
Shane Garrity <shanedgarrity@gmail.com>
2018-10-07 14:28:17 -0400

    complete override of material switch styles

--- src/components/Privacy/BrandSwitch.js
+++ src/components/Privacy/BrandSwitch.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-// import purple from '@material-ui/core/colors/purple';
-// import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@material-ui/core/Switch';
 
 const styles = theme => ({
   colorSwitchBase: {
-    // color: '#376e8c',
-    fontFamily: '"Share Tech Mono", monospace',
     '&$colorChecked': {
       color: '#376e8c',
       '& + $colorBar': {
@@ -18,18 +14,13 @@ const styles = theme => ({
     },
   },
   colorBar: {},
-  colorChecked: {}
+  colorChecked: {},
+  label: {
+    fontFamily: '"Share Tech Mono", monospace',
+  }
 });
 
 class BrandSwitch extends Component {
-  // state = {
-  //   checkedA: true,
-  //   checkedB: true,
-  // };
-
-  // handleChange = name => event => {
-  //   this.setState({ [name]: event.target.checked });
-  // };
 
   render() {
     const { classes } = this.props;
@@ -38,9 +29,6 @@ class BrandSwitch extends Component {
         <FormControlLabel
           control={
             <Switch
-              // checked={this.state.checkedA}
-              // onChange={this.handleChange('checkedA')}
-              // value="checkedA"
               checked={this.props.checked}
               onChange={this.props.onChange}
               value={this.props.value}
@@ -52,25 +40,10 @@ class BrandSwitch extends Component {
             />
           }
           label={this.props.label}
+          classes={{
+            label: classes.label
+          }}
         />
-        // <FormControlLabel
-        //   control={
-        //     <Switch
-        //       classes={{
-        //         switchBase: classes.iOSSwitchBase,
-        //         bar: classes.iOSBar,
-        //         icon: classes.iOSIcon,
-        //         iconChecked: classes.iOSIconChecked,
-        //         checked: classes.iOSChecked,
-        //       }}
-        //       disableRipple
-        //       checked={this.state.checkedB}
-        //       onChange={this.handleChange('checkedB')}
-        //       value="checkedB"
-        //     />
-        //   }
-        //   label="iOS style"
-        // />
     );
   }
 }
